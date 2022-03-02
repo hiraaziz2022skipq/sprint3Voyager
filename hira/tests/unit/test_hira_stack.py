445deeb2073a0202ba29e036ff50a7cfd4062c97
hira <hira.aziz.skipq@gmail.com>
2022-03-02 19:33:03 +0500

    10 commit

--- hira/tests/unit/test_hira_stack.py
+++ hira/tests/unit/test_hira_stack.py
@@ -1,15 +1,22 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from hira.hira_stack import HiraStack
+from sprint3Voyager.hira.hira.hira_stack import HiraStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in hira/hira_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = HiraStack(app, "hira")
-    template = assertions.Template.from_stack(stack)
+# def test_sqs_queue_created():
+#     app = core.App()
+#     stack = HiraStack(app, "hira")
+#     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
 #     })
+
+def test_s3bucket():
+    app = core.App()
+    stack = HiraStack(app, "hira")
+    template = assertions.Template.from_stack(stack)
+
+    template.resource_count_is("AWS::S3::Bucket", 1)
