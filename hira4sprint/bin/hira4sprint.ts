aaeff4dd72c33ea7b4c9687c3944c3ad14dae805
hira <hira.aziz.skipq@gmail.com>
2022-03-09 18:14:11 +0500

    changes

--- hira4sprint/bin/hira4sprint.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-// import { Hira4SprintStack } from '../lib/hira4sprint-stack';
-import {Hirapipelinestack} from '../lib/hirapipelinestack'
-const app = new cdk.App();
-new Hirapipelinestack(app, 'Hira4SprintStack');
