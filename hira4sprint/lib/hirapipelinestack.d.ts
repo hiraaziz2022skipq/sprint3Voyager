311776bf2dd51400358460e9a8618a1b6d5d3779
hira <hira.aziz.skipq@gmail.com>
2022-03-09 19:01:51 +0500

    again upload4

--- hira4sprint/lib/hirapipelinestack.d.ts
+++ hira4sprint/lib/hirapipelinestack.d.ts
@@ -2,5 +2,4 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 export declare class Hirapipelinestack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps);
-    create_role(): any;
 }
