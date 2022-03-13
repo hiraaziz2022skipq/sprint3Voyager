73cee5acbf3a3625c1d38d9eb49844308fdec138
hiraaziz2022 <hira.aziz.skipq@gmail.com>
2022-03-14 03:54:44 +0500

    sprint 5

---
+++ sprint5hira/test/sprint5hira.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sprint5Hira from '../lib/sprint5hira-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sprint5hira-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sprint5Hira.Sprint5HiraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
